b3e4312544e19c7a422ec304be0ac0b0fc8f4dae
am4am5 <36809996+am4am5@users.noreply.github.com>
2022-12-21 22:19:13 +0300

    Update repos.sh

--- repos.sh
+++ repos.sh
@@ -1,7 +1,8 @@
-bitnami         https://charts.bitnami.com/bitnami                                
-gitlab          https://charts.gitlab.io                                          
-grafana         https://grafana.github.io/helm-charts                             
-strimzi         https://strimzi.io/charts/                                        
-nginx-ingress   https://kubernetes.github.io/ingress-nginx
-nfs             https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
-loki            https://grafana.github.io/loki/charts
+helm repo add 
+bitnami https://charts.bitnami.com/bitnami                                
+gitlab https://charts.gitlab.io                                          
+grafana https://grafana.github.io/helm-charts                             
+strimzi https://strimzi.io/charts/                                        
+nginx-ingress https://kubernetes.github.io/ingress-nginx
+nfs https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
+loki https://grafana.github.io/loki/charts
