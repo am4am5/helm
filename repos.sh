33fdd2f30854668abdc5ac4ee0d4e0b4dc50200a
am4am5 <36809996+am4am5@users.noreply.github.com>
2022-12-21 22:21:42 +0300

    Update repos.sh

--- repos.sh
+++ repos.sh
@@ -5,4 +5,3 @@ grafana https://grafana.github.io/helm-charts
 strimzi https://strimzi.io/charts/                                        
 nginx-ingress https://kubernetes.github.io/ingress-nginx
 nfs https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
-loki https://grafana.github.io/loki/charts
