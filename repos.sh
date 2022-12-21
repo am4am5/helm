dd3ddfe2b9226f8501ad8a2639b3551f422567df
am4am5 <36809996+am4am5@users.noreply.github.com>
2022-12-21 22:16:44 +0300

    Update repos.sh

--- repos.sh
+++ repos.sh
@@ -2,9 +2,6 @@ bitnami         https://charts.bitnami.com/bitnami
 gitlab          https://charts.gitlab.io                                          
 grafana         https://grafana.github.io/helm-charts                             
 strimzi         https://strimzi.io/charts/                                        
-
-#no need for minikube
 nginx-ingress   https://kubernetes.github.io/ingress-nginx
 nfs             https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
-
 loki            https://grafana.github.io/loki/charts
