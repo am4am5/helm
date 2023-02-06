fffca7b9842471290485201e3d1bf5cf166b5a8b
am4am5 <36809996+am4am5@users.noreply.github.com>
2023-02-06 16:38:06 +0300

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,1 +1,1 @@
-wget https://get.helm.sh/helm-v3.10.3-linux-amd64.tar.gz && tar xvf helm-v3.10.3-linux-amd64.tar.gz && mv -f linux-amd64/helm /usr/local/bin/ && rm -rf linux-amd64/ helm-v* && helm version
+wget https://get.helm.sh/helm-v3.11.0-linux-amd64.tar.gz && tar xvf helm-v3.11.0-linux-amd64.tar.gz && mv -f linux-amd64/helm /usr/local/bin/ && rm -rf linux-amd64/ helm-v* && helm version
