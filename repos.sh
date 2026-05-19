6ccb7e42eac3b5010c74be3d6c8cfa8de784b22f
am4am5 <36809996+am4am5@users.noreply.github.com>
2026-05-19 17:42:09 +0300

    Update repos.sh

--- repos.sh
+++ repos.sh
@@ -5,3 +5,4 @@ grafana https://grafana.github.io/helm-charts
 strimzi https://strimzi.io/charts/                                        
 nginx-ingress https://kubernetes.github.io/ingress-nginx
 nfs https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
+valkey https://valkey.io/valkey-helm/
